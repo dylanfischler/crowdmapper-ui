8189eb081896fae2f849dad5712a3772089a3125
Zachary Milrod <zmilrod@gmail.com>
2016-12-13 20:24:35 -0500

    added drawing curves

--- public/js/main.js
+++ public/js/main.js
@@ -36,12 +36,76 @@
     return new Promise(function(resolve, reject) {
       axios.get('/api/curves').then(function(response) {
         self.curves = response.data;
-        debugger;
+        self.drawCurves();
         resolve();
       }).catch(function(error) {
         reject(error);
       });
     });
+  }
+
+  function calcDistance(locationOne, locationTwo){
+    var radius = 6378137;   // approximate Earth radius, *in meters*
+        var deltaLat = locationOne[0] - locationTwo[0];
+        var deltaLon = locationOne[1] - locationTwo[1];
+        var angle = 2 * Math.asin( Math.sqrt(
+        Math.pow(Math.sin(deltaLat/2), 2) + 
+        Math.cos(locationTwo[0]) * Math.cos(locationOne[0]) * 
+        Math.pow(Math.sin(deltaLon/2), 2) ) );
+        return radius * angle;
+  }
+
+  CrowdMapper.prototype.drawCurves = function() {
+    var self = this;
+    self.clearMarkers();
+    self.mapBounds = new google.maps.LatLngBounds();
+    for(curve in self.curves){
+      var path = [];
+      var lastLat = 0;
+      var lastLng = 0;
+      var lastPoint = [0,0];
+      self.curves[curve].sort(function(locationOne, locationTwo){
+        if((locationOne[0] - locationTwo[0]) > 0){
+          if( (locationOne[1] - locationTwo[1]) > 0 ){
+            return 1;
+          }
+        }
+        else{
+          if( (locationOne[1] - locationTwo[1]) < 0 ){
+            return -1;
+          }
+        }
+        return -(locationOne[0] - locationTwo[0]) + (locationOne[1] - locationTwo[1]);
+      });
+      self.curves[curve].forEach(function(location) {
+        if(location[0] != null){
+          // if(Math.abs(lastLat - location[0]) > 0.00001 && Math.abs(lastLng - location[1]) > 0.00001){
+            var latLng = new google.maps.LatLng(location[0], location[1]);
+            path.push(latLng);
+            // var marker = new google.maps.Marker({
+            //   position: latLng,
+            //   map: self.map
+            // });
+            // self.markers.push(marker)
+            self.mapBounds.extend(latLng);
+            lastLat = location[0];
+            lastLng = location[1];
+          // }
+        }
+      });
+
+      var newPath = new google.maps.Polyline({
+          path: path,
+          geodesic: true,
+          strokeColor: getRandomColor(),
+          strokeOpacity: 1.0,
+          strokeWeight: 2,
+          fillOpacity: 0.0
+        });
+      newPath.setMap(self.map);
+    }
+
+    self.map.fitBounds(self.mapBounds);
   }
 
   CrowdMapper.prototype.getClusters = function() {
